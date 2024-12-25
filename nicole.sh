351b7a94904ba45862835b7aec9c107614eea520
evian <alu0101395548@ull.edu.es>
2024-12-25 23:02:21 +0000

    first test using catch2

--- nicole.sh
+++ nicole.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+# Crear el directorio build si no existe
 if [ ! -d "./build" ]; then
     mkdir build
     cd build
     cmake ..
-    make run
+    make
+    cd ..
 fi
 
-cd build
+# Comprobar si se pasa la opción -t
+if [[ "$1" == "-t" ]]; then
+    cd build
+    make
+    # Ejecutar los tests
+    ctest --verbose
+    exit 0
+fi
 
+# Compilar el proyecto
+cd build
 make
 
+# Volver al directorio principal
 cd ..
 
+# Ejecutar el binario principal con los argumentos proporcionados
 ./build/bin/binaries "$@"
---
+++ test/test.cc
@@ -0,0 +1,17 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "../inc/lexicalAnalysis/location.h"
+
+uint32_t factorial( uint32_t number ) {
+    return number <= 1 ? number : factorial(number-1) * number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( factorial( 1) == 1 );
+    REQUIRE( factorial( 2) == 2 );
+    REQUIRE( factorial( 3) == 6 );
+    REQUIRE( factorial(10) == 3'628'800 );
+    REQUIRE( nicole::Location{"", 10, 10}.col() == 10);
+    REQUIRE( nicole::Location{"", 10, 10}.col() == 10);
+    REQUIRE( nicole::Location{"", 10, 10}.col() == 10);
+} 
