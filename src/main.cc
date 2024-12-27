f85d6e482389e2e23a990a0fa92821b910ba1502
evian <alu0101395548@ull.edu.es>
2024-12-27 20:54:09 +0000

    small fix

--- src/main.cc
+++ src/main.cc
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
               << " ---> Raw: " << token.raw()
               << " ---> Loc: " << token.locInfo() << "\n";
   }
-  return 0;
+  return EXIT_SUCCESS;
 }
 
 /*
