67691d3ea2adce5f269c0a16c142d2a1b41edd03
Evian <alu0101395548@ull.edu.es>
2025-01-18 19:27:06 +0000

    parsing

--- src/parsingAnalysis/algorithm/productions/parseFactor.cc
+++ src/parsingAnalysis/algorithm/productions/parseFactor.cc
@@ -14,12 +14,14 @@ TopDown::parseFactor() const noexcept {
     }
     return Builder::createBool(true);
   }
+  
   case TokenType::FALSE: {
     if (!tkStream_.eat()) {
       break;
     }
     return Builder::createBool(false);
   }
+
   case TokenType::NUMBER_INT: {
     const int value{std::stoi(raw)};
     if (!tkStream_.eat()) {
@@ -27,6 +29,7 @@ TopDown::parseFactor() const noexcept {
     }
     return Builder::createInt(value);
   }
+
   case TokenType::NUMBER_DOUBLE: {
     const double value{std::stod(raw)};
     if (!tkStream_.eat()) {
@@ -34,6 +37,7 @@ TopDown::parseFactor() const noexcept {
     }
     return Builder::createDouble(value);
   }
+
   case TokenType::NUMBER_FLOAT: {
     const float value{std::stof(raw.substr(1))};
     if (!tkStream_.eat()) {
@@ -41,6 +45,7 @@ TopDown::parseFactor() const noexcept {
     }
     return Builder::createFloat(value);
   }
+
   case TokenType::STRING: {
     const std::string value{raw};
     if (!tkStream_.eat()) {
@@ -48,6 +53,7 @@ TopDown::parseFactor() const noexcept {
     }
     return Builder::createString(value);
   }
+
   case TokenType::CHAR: {
     const std::string value{raw};
     if (!tkStream_.eat()) {
@@ -55,42 +61,107 @@ TopDown::parseFactor() const noexcept {
     }
     return Builder::createChar(value);
   }
+
   case TokenType::NULLPTR: {
     if (!tkStream_.eat()) {
       break;
     }
     return Builder::createNull();
   }
+
   case TokenType::LP: {
     if (!tkStream_.eat()) {
       break;
     }
-
     if (tkStream_.current()->type() == TokenType::RP) {
       return std::unexpected{
           Error{ERROR_TYPE::SINTAX, "empty expression at " + loc}};
     }
-
     const std::expected<std::shared_ptr<AST>, Error> expression{parseOr()};
-
     if (!expression || !*expression) {
       return std::unexpected{expression
                                  ? Error{ERROR_TYPE::NULL_NODE, "node is null"}
                                  : expression.error()};
     }
-
     if (!(tkStream_.current()->type() == TokenType::RP)) {
       return std::unexpected{
           Error{ERROR_TYPE::SINTAX, "missing right parenthesis at " +
                                         tkStream_.current()->locInfo()}};
     }
-
     if (!tkStream_.eat()) {
       break;
     }
-
     return expression;
   }
+
+  case TokenType::OPERATOR_SUB: {
+    const Token token{*tkStream_.current()};
+    if (!tkStream_.eat()) {
+      break;
+    }
+    const std::expected<std::shared_ptr<AST>, Error> expression{parseOr()};
+    if (!expression || !*expression) {
+      return std::unexpected{expression
+                                 ? Error{ERROR_TYPE::NULL_NODE, "node is null"}
+                                 : expression.error()};
+    }
+    if (!tkStream_.eat()) {
+      break;
+    }
+    return Builder::createNeg(token, *expression);
+  }
+
+  case TokenType::OPERATOR_NOT: {
+    const Token token{*tkStream_.current()};
+    if (!tkStream_.eat()) {
+      break;
+    }
+    const std::expected<std::shared_ptr<AST>, Error> expression{parseOr()};
+    if (!expression || !*expression) {
+      return std::unexpected{expression
+                                 ? Error{ERROR_TYPE::NULL_NODE, "node is null"}
+                                 : expression.error()};
+    }
+    if (!tkStream_.eat()) {
+      break;
+    }
+    return Builder::createNot(token, *expression);
+  }
+
+  case TokenType::DECREMENT: {
+    const Token token{*tkStream_.current()};
+    if (!tkStream_.eat()) {
+      break;
+    }
+    const std::expected<std::shared_ptr<AST>, Error> expression{parseOr()};
+    if (!expression || !*expression) {
+      return std::unexpected{expression
+                                 ? Error{ERROR_TYPE::NULL_NODE, "node is null"}
+                                 : expression.error()};
+    }
+    if (!tkStream_.eat()) {
+      break;
+    }
+    return Builder::createDecrement(token, *expression);
+  }
+
+  case TokenType::INCREMENT: {
+    const Token token{*tkStream_.current()};
+    if (!tkStream_.eat()) {
+      break;
+    }
+    const std::expected<std::shared_ptr<AST>, Error> expression{parseOr()};
+    if (!expression || !*expression) {
+      return std::unexpected{expression
+                                 ? Error{ERROR_TYPE::NULL_NODE, "node is null"}
+                                 : expression.error()};
+    }
+    if (!tkStream_.eat()) {
+      break;
+    }
+    return Builder::createIncrement(token, *expression);
+  }
+
   default:
     // error
     return std::unexpected<Error>{
