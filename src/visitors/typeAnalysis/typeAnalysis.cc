50ef1fb48d2d5c65c51bd135cf6b4b8d97f55986
Evian <alu0101395548@ull.edu.es>
2025-03-25 21:15:38 +0000

    indicaciones anotadas para cada nodo en el type analisis

--- src/visitors/typeAnalysis/typeAnalysis.cc
+++ src/visitors/typeAnalysis/typeAnalysis.cc
@@ -55,6 +55,7 @@
 
 #include "../../../inc/parsingAnalysis/ast/tree.h"
 #include <cstddef>
+#include <iostream>
 #include <memory>
 #include <variant>
 
@@ -128,144 +129,134 @@ TypeAnalysis::visit(const AST_STRING *node) const noexcept {
   return typeTable_->getType("str");
 }
 
+/*
+- si esta vacio retorna un vector<null>
+- si no esta vacio, comprueba que todos los elementos sean de un mismo tipo
+- si un elemento es un puntero userType debe comprobar que todos parten de una
+misma base
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_VECTOR *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_VECTOR");
   }
-  if (node->values().empty()) {
-    return std::make_shared<VectorType>(typeTable_->null());
-  }
-  const auto values{node->values()};
-  const auto type{values[0]->accept(*this)};
-  if (!type) {
-    return createError(type.error());
-  }
-  for (const auto &value : values) {
-    const auto result{value->accept(*this)};
-    if (!result) {
-      return createError(result.error());
-    }
-    if (!typeTable_->areSameType(*type, *result)) {
-    }
-  }
   return {};
 }
 
+/*
+- el value debe ser de tipo int
+- se apoya en currentType para deducir el tipo del elemento al que accede
+ */
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_INDEX *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_INDEX");
   }
-  const auto result{node->index()->accept(*this)};
-  if (!result) {
-    return createError(result.error());
-  }
   return {};
 }
 
+/*
+- value debe ser un puntero y que este en la tabla de memoria dinamica
+- retorna NoPropagate
+ */
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_DELETE *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_DELETE");
   }
-  const auto result{node->value()->accept(*this)};
-  if (!result) {
-    return createError(result.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- envuelve en un puntero la expression y lo retorna
+ */
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_NEW *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_NEW");
   }
-  const auto result{node->value()->accept(*this)};
-  if (!result) {
-    return createError(result.error());
-  }
   return {};
 }
 
+/*
+- la expresion debe ser un puntero y retorna el tipo al que apunta
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_DEREF *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_DEREF");
   }
-  const auto result{node->value()->accept(*this)};
-  if (!result) {
-    return createError(result.error());
-  }
   return {};
 }
 
+/*
+- comrpueba que left no sea void ni NoPropagate, BreakType
+- comrpueba que right no sea void ni NoPropagate, BreakType
+- comrprueba que se puedan operar, caso speciales si un userType busca metodo
+para sobrecarga de operador
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_BINARY *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_BINARY");
   }
-  const auto left{node->left()->accept(*this)};
-  if (!left) {
-    return createError(left.error());
-  }
-  const auto right{node->right()->accept(*this)};
-  if (!right) {
-    return createError(right.error());
-  }
   return {};
 }
 
+/*
+- comprueba que expression no es void ni NoPropagate, BreakType
+- comrpueba que se puede operar, caso speciales si un userType busca metodo
+para sobrecarga de operador y retorna el tipo
+ */
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_UNARY *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_UNARY");
   }
-  const auto value{node->value()->accept(*this)};
-  if (!value) {
-    return createError(value.error());
-  }
   return {};
 }
 
+/*
+- comprueba que left no sea void ni NoPropagate, BreakType
+- comrpueba que right no sea void ni NoPropagate, BreakType
+- comprueba que sea asignable
+- retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_ASSIGNMENT *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_ASSIGNMENT");
   }
-  const auto left{node->left()->accept(*this)};
-  if (!left) {
-    return createError(left.error());
-  }
-  const auto right{node->value()->accept(*this)};
-  if (!right) {
-    return createError(right.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- comprueba que los tipos de los argumentos no sean void ni NoPropagate, BreakType
+- si es un userType comprueba que exista el metodo toStr()
+- retorna NoPropagate
+ */
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_PRINT *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_PRINT");
   }
-  for (const auto &stm : node->values()) {
-    const auto result{stm->accept(*this)};
-    if (!result) {
-      return createError(result.error());
-    }
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_IMPORT *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_IMPORT");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- retorna el tipo de la expresion
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_STATEMENT *node) const noexcept {
   if (!node) {
@@ -274,132 +265,157 @@ TypeAnalysis::visit(const AST_STATEMENT *node) const noexcept {
   return node->expression()->accept(*this);
 }
 
+/*
+- si encuentra pass y stop devuelve void
+- si encuentra un return retorna su tipo, vijilar varios returns en diferentes
+body anidados
+- si no encuentra nada retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_BODY *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_BODY");
   }
-  for (const auto &stm : node->body()) {
-    const auto result{stm->accept(*this)};
-    if (!result) {
-      return createError(result.error());
-    }
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- condtion debe retornar bool
+- si body encuentra algo diferente a NoPropagate lo propaga si no retorna
+NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_WHILE *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_WHILE");
   }
-  const auto condition{node->condition()->accept(*this)};
-  if (!condition) {
-    return createError(condition.error());
-  }
-  const auto body{node->body()->accept(*this)};
-  if (!body) {
-    return createError(body.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- init ???
+- condtion reotrna bool
+- update ???
+- body lo mismo que los otros
+ */
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_FOR *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_FOR");
   }
-  const auto condition{node->condition()->accept(*this)};
-  if (!condition) {
-    return createError(condition.error());
-  }
-  const auto body{node->body()->accept(*this)};
-  if (!body) {
-    return createError(body.error());
-  }
   return {};
 }
 
+/*
+- si body encuentra algo diferente a NoPropagate lo propaga si no retorna
+NoPropagate
+- condtion debe retornar bool
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_DO_WHILE *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_DO_WHILE");
   }
-  const auto body{node->body()->accept(*this)};
-  if (!body) {
-    return createError(body.error());
-  }
-  const auto condition{node->condition()->accept(*this)};
-  if (!condition) {
-    return createError(condition.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- retorna BreakType
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_PASS *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_PASS");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- retorna BreakType
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_STOP *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_STOP");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- codition debe ser bool
+- body lo mismo que el resto
+- else if lo mismo que if
+- else lo mismo que body
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_IF *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_IF");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- lo mismo que if
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_ELSE_IF *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_ELSE_IF");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- match solo bool, int, char
+- Lo mismo que if
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_SWITCH *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_SWITCH");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- match solo bool, int char
+- igual que body
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_CASE *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_CASE");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- igual que body
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_DEFAULT *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_DEFAULT");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- condtion debe ser bool
+- first y second no pueden ser breakType
+ */
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_TERNARY *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_TERNARY");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- debe retornar bool
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_CONDITION *node) const noexcept {
   if (!node) {
@@ -408,6 +424,13 @@ TypeAnalysis::visit(const AST_CONDITION *node) const noexcept {
   return typeTable_->getType("bool");
 }
 
+/*
+- numero correcto de generics en caso de tener
+- parametros son del tipo correcto (no pueden ser void ni punteros a este)
+- body debe retornar un tipo compatible al tipo de la funcion
+- si no tiene generics debe existir una función con esos argumentos
+- retorna tipo de función
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_FUNC_CALL *node) const noexcept {
   if (!node) {
@@ -416,14 +439,24 @@ TypeAnalysis::visit(const AST_FUNC_CALL *node) const noexcept {
   return {};
 }
 
+/*
+- parametros no pueden ser void ni punteros a este
+- comprobar si algo es generic
+- body debe retornar un tipo compatible al tipo de la funcion
+- retorna el tipo NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_FUNC_DECL *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_FUNC_DECL");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- si esta vacio retorna void
+- si no el tipo de la expresion
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_RETURN *node) const noexcept {
   if (!node) {
@@ -432,14 +465,20 @@ TypeAnalysis::visit(const AST_RETURN *node) const noexcept {
   return {};
 }
 
+/*
+- retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_ENUM *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_ENUM");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- retorna int
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_ENUM_ACCESS *node) const noexcept {
   if (!node) {
@@ -448,14 +487,25 @@ TypeAnalysis::visit(const AST_ENUM_ACCESS *node) const noexcept {
   return typeTable_->getType("int");
 }
 
+/*
+- comprobar tipo atributos no sea void ni puntero a est, comprobar si es generic
+- comrpobar metodos
+- comproabr constructor
+- comprobar destructor
+- retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_STRUCT *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_STRUCT");
   }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- comprobar que sea genric
+- se apoya en currentType para saber si el atributo existe y retorna el tipo
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_ATTR_ACCESS *node) const noexcept {
   if (!node) {
@@ -464,6 +514,11 @@ TypeAnalysis::visit(const AST_ATTR_ACCESS *node) const noexcept {
   return {};
 }
 
+/*
+- tener en cuenta generics
+- se apoya en currentType para saber si el metodo existe y retorna el tipo
+- igual que funcion
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_METHOD_CALL *node) const noexcept {
   if (!node) {
@@ -472,30 +527,32 @@ TypeAnalysis::visit(const AST_METHOD_CALL *node) const noexcept {
   return {};
 }
 
+/*
+- igual que funcion
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_METHOD_DECL *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "Invalid AST_METHOD_DECL");
   }
-  const auto body{node->body()->accept(*this)};
-  if (!body) {
-    return createError(body.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- igual que funcion
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_CONSTRUCTOR_DECL *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "Invalid AST_CONSTRUCTOR_DECL");
   }
-  const auto body{node->body()->accept(*this)};
-  if (!body) {
-    return createError(body.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- retorna el tipo del padre
+- comprobar generics y argumentos
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_SUPER *node) const noexcept {
   if (!node) {
@@ -504,18 +561,21 @@ TypeAnalysis::visit(const AST_SUPER *node) const noexcept {
   return typeTable_->getType(node->fatherType()->toString());
 }
 
+/*
+- comprobar body
+- retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_DESTRUCTOR_DECL *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "Invalid AST_DESTRUCTOR_DECL");
   }
-  const auto body{node->body()->accept(*this)};
-  if (!body) {
-    return createError(body.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- retorna el tipo de la clase envuelto en un puntero const?
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_THIS *node) const noexcept {
   if (!node) {
@@ -524,6 +584,11 @@ TypeAnalysis::visit(const AST_THIS *node) const noexcept {
   return node->userType();
 }
 
+/*
+- comprueba generics
+- comprueba argumentos
+- comprueba body
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_CONSTRUCTOR_CALL *node) const noexcept {
   if (!node) {
@@ -532,57 +597,47 @@ TypeAnalysis::visit(const AST_CONSTRUCTOR_CALL *node) const noexcept {
   return typeTable_->getType(node->id());
 }
 
+/*
+- comrpueba que el tipo de la expresion no sea void
+- fija en currentScope el tipo
+- retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_AUTO_DECL *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_AUTO_DECL");
   }
-  const auto expression{node->value()->accept(*this)};
-  if (!expression) {
-    return createError(expression.error());
-  }
-  if (typeTable_->areSameType(*typeTable_->getType("void"), *expression)) {
-    return createError(
-        ERROR_TYPE::TYPE,
-        "an auto declaration cannot be assigned to a void expression");
-  }
-  const auto setType{currentScope_->setVariableType(node->id(), *expression)};
-  if (!setType) {
-    return createError(setType.error());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- comrpueba que el tipo de la expresion no sea body
+- comrpueba que la expresion sea compatible
+- retorna NoPropagate
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_VAR_TYPED_DECL *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_VAR_TYPED_DECL");
   }
-  const auto expression{node->value()->accept(*this)};
-  if (!expression) {
-    return createError(expression.error());
-  }
-  if (!typeTable_->areSameType(node->varType(), *expression)) {
-    return createError(
-        ERROR_TYPE::TYPE,
-        "the variable type is: " + node->varType()->toString() +
-            " but the expression is: " + expression.value()->toString());
-  }
-  return typeTable_->getType("void");
+  return {};
 }
 
+/*
+- se apoya en currentScope
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_VAR_CALL *node) const noexcept {
   if (!node) {
     return createError(ERROR_TYPE::NULL_NODE, "invalid AST_VAR_CALL");
   }
-  const auto var{currentScope_->getVariable(node->id())};
-  if (!var) {
-    return createError(var.error());
-  }
-  return var->type();
+  return {};
 }
 
+/*
+- comprueba que base no sea void
+- retorna ultimo tipo encontrado
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const AST_CHAINED *node) const noexcept {
   if (!node) {
@@ -591,6 +646,10 @@ TypeAnalysis::visit(const AST_CHAINED *node) const noexcept {
   return {};
 }
 
+/*
+- comprueba que no hubo errores
+- retorna int / void
+*/
 std::expected<std::shared_ptr<Type>, Error>
 TypeAnalysis::visit(const Tree *tree) const noexcept {
   if (!tree) {
