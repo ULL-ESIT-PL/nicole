b0956317946aaa1c7e6191a066f96bf2bb73e838
Evian <alu0101395548@ull.edu.es>
2025-03-30 16:33:48 +0100

    developing type analysis

--- src/tables/typeTable/typeValidators/strValidator.cc
+++ src/tables/typeTable/typeValidators/strValidator.cc
@@ -3,7 +3,27 @@
 namespace nicole {
 
 std::shared_ptr<Type>
-StrValidator::isAssignable(const std::shared_ptr<Type> &) const noexcept {
+StrValidator::isAssignable(const std::shared_ptr<Type> &type) const noexcept {
+    if (const auto basicType{std::dynamic_pointer_cast<BasicType>(type)}) {
+    switch (basicType->baseKind()) {
+    case BasicKind::Str:
+    case BasicKind::Char:
+      return std::make_shared<BasicType>(BasicKind::Str);
+    default:
+      return nullptr;
+    }
+  } else if (const auto constType{std::dynamic_pointer_cast<ConstType>(type)}) {
+    if (const auto basicType2{
+            std::dynamic_pointer_cast<BasicType>(constType->baseType())}) {
+      switch (basicType2->baseKind()) {
+      case BasicKind::Str:
+      case BasicKind::Char:
+        return std::make_shared<BasicType>(BasicKind::Str);
+      default:
+        return nullptr;
+      }
+    }
+  }
   return nullptr;
 }
 
